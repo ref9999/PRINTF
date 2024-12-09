b3fc2163885f127c39eb83e59bd595d2f6cf216f
Riad El Fassi <riel-fas@c2r5p8.1337.ma>
2024-12-09 17:42:00 +0100

    printf

--- main.c
+++ main.c
@@ -51,34 +51,47 @@
 
 #include <stdio.h>
 #include "ft_printf.h"
+#include <stdlib.h>
 
 int main(void)
 {
-    // Test cases for ft_printf
-    ft_printf("ft_printf: Hello, World!\n");
-    ft_printf("ft_printf: Character: %c\n", 'A');
-    ft_printf("ft_printf: String: %s\n", "Test String");
-    ft_printf("ft_printf: Decimal: %d\n", 123);
-    ft_printf("ft_printf: Integer: %i\n", -456);
-    ft_printf("ft_printf: Unsigned: %u\n", 789);
-    ft_printf("ft_printf: Pointer: %p\n", (void *)0x12345678);
-    ft_printf("ft_printf: Hexadecimal (lowercase): %x\n", 0xabc);
-    ft_printf("ft_printf: Hexadecimal (uppercase): %X\n", 0xABC);
-    ft_printf("ft_printf: Percent sign: %%\n");
 
-    printf("\n");
+	//close (1);
+
+	int ret = 0;
+    // Test cases for ft_printf
+    // ft_printf("ft_printf: Hello, World!\n");
+    // ft_printf("ft_printf: Character: %c\n", 'A');
+    // ft_printf("ft_printf: String: %s\n", "Test String");
+    // ft_printf("ft_printf: Decimal: %d\n", 123);
+    // ft_printf("ft_printf: Integer: %i\n", -456);
+    // ft_printf("ft_printf: Unsigned: %u\n", 789);
+    // ft_printf("ft_printf: Pointer: %p\n", (void *)0x12345678);
+    // ft_printf("ft_printf: Hexadecimal (lowercase): %x\n", 0xabc);
+    // ft_printf("ft_printf: Hexadecimal (uppercase): %X\n", 0xABC);
+    // ft_printf("ft_printf: Percent sign: %%\n");
+
+    //printf("\n");
 
     // Test cases for printf
-    printf("printf: Hello, World!\n");
-    printf("printf: Character: %c\n", 'A');
-    printf("printf: String: %s\n", "Test String");
-    printf("printf: Decimal: %d\n", 123);
-    printf("printf: Integer: %i\n", -456);
-    printf("printf: Unsigned: %u\n", 789);
-    printf("printf: Pointer: %p\n", (void *)0x12345678);
-    printf("printf: Hexadecimal (lowercase): %x\n", 0xabc);
-    printf("printf: Hexadecimal (uppercase): %X\n", 0xABC);
-    printf("printf: Percent sign: %%\n");
+    // printf("printf: Hello, World!\n");
+    // printf("printf: Character: %c\n", 'A');
+    // printf("printf: String: %s\n", "Test String");
+    // printf("printf: Decimal: %d\n", 123);
+    // printf("printf: Integer: %i\n", -456);
+    // printf("printf: Unsigned: %u\n", 789);
+    // printf("printf: Pointer: %p\n", (void *)0x12345678);
+    // printf("printf: Hexadecimal (lowercase): %x\n", 0xabc);
+    // printf("printf: Hexadecimal (uppercase): %X\n", 0xABC);
+    // ret = printf("pgr %%\n");
+	// ret += 48;
+	// write(2, &ret, 1);
+	// write(2, "\n", 1);
+	ret = ft_printf(" %");
+	ret += 48;
+	write(2, "the: \n", 6);
+	write(2, &ret, 1);
+	write(2, "\n", 1);
 
-    return 0;
+    return (0);
 }
